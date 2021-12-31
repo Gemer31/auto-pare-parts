f68cdb1b921589c453b7bcef752cdccfe7e9de43
rakr0819 <raman.krauchanka@netcracker.com>
2021-12-31 21:09:20 +0300

    start auto pare parts app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,137 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit, ViewEncapsulation } from '@angular/core';
+
+export interface TableRow {
+  name: string;
+  position: number;
+  secondHandMinPrice?: number;
+  secondHandAveragePrice?: number;
+  secondHandMaxPrice?: number;
+  newMinPrice?: number;
+  newAveragePrice?: number;
+  newMaxPrice?: number;
+}
+
+export interface SelectValue {
+  value?: string;
+  text?: string;
+}
+
+const ELEMENT_DATA: TableRow[] = [
+  {
+    position: 1,
+    name: 'Капот',
+    secondHandMinPrice: 12,
+    secondHandAveragePrice: 24,
+    secondHandMaxPrice: 36,
+    newMinPrice: 15,
+    newAveragePrice: 30,
+    newMaxPrice: 45,
+  },
+  {
+    position: 2,
+    name: 'Бампер',
+    secondHandMinPrice: 12,
+    secondHandAveragePrice: 24,
+    secondHandMaxPrice: 36,
+    newMinPrice: 15,
+    newAveragePrice: 30,
+    newMaxPrice: 45,
+  },
+  {
+    position: 3,
+    name: 'Крыло',
+    secondHandMinPrice: 12,
+    secondHandAveragePrice: 24,
+    secondHandMaxPrice: 36,
+  },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'auto-pare-parts';
+export class AppComponent implements OnInit {
+  @HostBinding("class.app") public hostClass: boolean = true;
+
+  public defaultURL: string = "https://bamper.by/"
+
+  public _markas: SelectValue[] = [];
+  public _pareParts: SelectValue[] = [];
+  public _models: SelectValue[] = [];
+
+  public _selectedMarka: SelectValue = {};
+  public _selectedParePart: SelectValue = {};
+  public _selectedModel: SelectValue = {};
+
+  public _tableRows: TableRow[] = [];
+
+  tableColumns: string[] = ['position', 'name', 'secondHandMinPrice'];
+  dataSource = ELEMENT_DATA;
+
+  secondHandMinPrice?: number;
+  secondHandAveragePrice?: number;
+  secondHandMaxPrice?: number;
+  newMinPrice?: number;
+  newAveragePrice?: number;
+  newMaxPrice?: number;
+
+  ngOnInit() {
+    fetch(this.defaultURL)
+      .then((response: Response) => (response.text()))
+      .then((htmlAsString: string) => {
+        const html: DocumentFragment = document.createRange().createContextualFragment(htmlAsString);
+
+        html.getElementById("marka")?.querySelectorAll("option").forEach((item: HTMLOptionElement) => {
+          this._markas.push({value: item.value, text: item.innerText});
+        });
+
+        html.getElementById("zapchast")?.querySelectorAll("option").forEach((item: HTMLOptionElement) => {
+          this._pareParts.push({value: item.value, text: item.innerText});
+        });
+      });
+  }
+
+  public _markaChanged(event: any): void {
+    fetch(this.defaultURL + `zchbu/marka_${event}/`)
+      .then((response: Response) => (response.text()))
+      .then((htmlAsString: string) => {
+        const html: DocumentFragment = document.createRange().createContextualFragment(htmlAsString);
+
+        html.getElementById("model")?.querySelectorAll("option").forEach((item) => {
+          this._models.push({value: item.value, text: item.innerText});
+        });
+      });
+  }
+
+  private prepareUrl(): string {
+    let url: string = this.defaultURL + "zchbu/";
+
+    if (this._selectedParePart?.value) {
+      url += `zapchast_${this._selectedParePart.value}/`;
+    }
+    if (this._selectedMarka?.value) {
+      url += `marka_${this._selectedMarka.value}/`;
+    }
+    if (this._selectedModel?.value) {
+      url += `model_${this._selectedMarka.value}/`;
+    }
+
+    return url;
+  }
+
+  public _calculateClicked(): void {
+    fetch(this.prepareUrl())
+      .then((response: Response) => (response.text()))
+      .then((htmlAsString: string) => {
+        const html: DocumentFragment = document.createRange().createContextualFragment(htmlAsString);
+
+        html.getElementById("model")?.querySelectorAll("option").forEach((item) => {
+          this._models.push({value: item.value, text: item.innerText});
+        });
+      });
+  }
+
+  class="modern-page-next"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from "@angular/common";
+import { MatCommonModule } from "@angular/material/core";
+import { MatSelectModule } from "@angular/material/select";
+import { MatTableModule } from "@angular/material/table";
+import { HttpClientModule } from "@angular/common/http";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    MatCommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatTableModule,
+    HttpClientModule,
+    MatButtonModule,
+    NgModule
   ],
-  providers: [],
+  providers: [
+    XMLHttpRequest,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
